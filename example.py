f2b52fa3f205fd5c6a92b3f2cce657278caa4b13
Gregory Sacco <gregorysacco@MacBook-Pro-6.local>
2023-05-09 13:07:47 -0500

    Restored previous version

--- example.py
+++ example.py
@@ -1,4 +1,3 @@
-import random
 import dash
 from dash import html, dcc, ctx
 from dash.dependencies import Output, Input, State
@@ -16,8 +15,8 @@ inlet=StreamInlet(streams[0])
 streams=resolve_stream('name','plot_data_GP')
 inlet_gp=StreamInlet(streams[0])
 
-streams=resolve_stream('name','plot_data_acq')
-inlet_acq=StreamInlet(streams[0])
+# streams=resolve_stream('name','plot_data_acq')
+# inlet_acq=StreamInlet(streams[0])
 
 streams=resolve_stream('name','polar ECG')
 inlet_ecg=StreamInlet(streams[0])
@@ -40,6 +39,8 @@ class MyClass:
         self.Acq_data_plot = None
         self.GP_data_plot = None
         self.ECGy =[]
+        self.HistParm=[]
+        self.HistGP = []
         
 values = MyClass()
 
@@ -144,7 +145,7 @@ def render_content(tab):
                 html.Br(),
                 html.Div([
                     html.Br(),
-                    #html.H3('Cost/iterations'),
+                    html.H3('Biofeedback signal'),
                     html.Br(),
                     dcc.Graph(id='live_ECG')
                 ], style={'padding':20, 'flex':1}),
@@ -176,8 +177,8 @@ def update_graph(n):
     [costMin, costMax]= config['Optimization']['range_cost']
     n_parm = config['Optimization']['n_parms']
 
-    data_plot, time_inlet = inlet.pull_chunk(timeout=0.2)
-    print('before numpy', data_plot)
+    data_plot, time_inlet = inlet.pull_sample(timeout=0.2)
+    # print(data_plot, time_inlet)
 
     data_gp, time_inlet_gp = inlet_gp.pull_chunk(timeout=0.2)
     if len(time_inlet_gp):
@@ -189,88 +190,75 @@ def update_graph(n):
             if time_inlet is not None:
                 values.parameter1.append(data_plot[1])
                 values.GPy.append(data_plot[0])
-     
             layout = go.Layout(xaxis = dict(title='Parameter',range=[parmMin, parmMax]),
-                yaxis=dict(title='Cost'),title='Gaussian Process')
+                                    yaxis=dict(title='Cost'),title='Gaussian Process')
             data = go.Scatter(x=list(values.parameter1), y=list(values.GPy), name='cost_samples', mode="markers")
             if values.GP_data_plot is not None:
                 data_1 = go.Scatter(x=list(np.linspace(0,85, 100)), y=values.GP_data_plot, name='GP', mode="lines")
-            return {'data':[data, data_1], 'layout':layout}
-        
-        case 2:
-            if time_inlet is not None:
-                if len(data_plot) > 1:
-                    data_plot_arr = np.array(data_plot)
-                    # divide the array into 3 columns
-                    cost = data_plot_arr[:len(data_plot_arr)//3]
-                    parm1 = data_plot_arr[len(data_plot_arr)//3:2*len(data_plot_arr)//3]
-                    parm2 = data_plot_arr[2*len(data_plot_arr)//3:len(data_plot_arr)]
-                    print('data_plot_arr', data_plot_arr)
-                    values.parameter1 = list(parm1.flatten())
-                    values.parameter2 = list(parm2.flatten())
-                    values.GPy = cost.flatten()
-                    # print(values.parameter1.shape, values.parameter2.shape, values.GPy.shape)
-
-            layout = go.Layout(scene = dict(xaxis_title='Parameter 1',yaxis_title='Parameter 2',
-                                zaxis_title='Cost'), width=600 , margin=dict(r=2, b=2, l=2, t=2))
-            
-            print('parameter 1', values.parameter1, 'parameter 2', values.parameter2, 'cost', values.GPy)
-            data=go.Scatter3d(x=list(values.parameter1), y=list(values.parameter2), z=list(values.GPy), name='cost_samples', mode="markers", )
-            if values.GP_data_plot is not None:
-                nx,ny = (20,20)
-                x = np.linspace(0,0.85, nx)
-                y= np.linspace(0,0.85, ny)
-                xv, yv = np.meshgrid(x,y)
-                data_1 = go.Mesh3d(x=xv.flatten(), y=yv.flatten(), z=list(values.GP_data_plot), name='GP',opacity=0.50)
-                 
-                return {'data':[data, data_1], 'layout':layout} 
-            return {'data':[data], 'layout':layout} 
-        
-
-@app.callback(Output(component_id="live_Acq", component_property="figure"), 
-              Input('graph-update', 'n_intervals'))   
-def update_graph(n):
-
-    [parmMin, parmMax]= config['Optimization']['range'][0]
-    [costMin, costMax]= config['Optimization']['range_cost']
-    n_parm = config['Optimization']['n_parms']
-
-    data_plot, time_inlet = inlet.pull_sample(timeout=0.2)
-    data_acq, time_inlet_acq = inlet_acq.pull_chunk(timeout=0.2)
-    if len(time_inlet_acq):
-        acq_list = [i[0] for i in data_acq]
-        values.Acq_data_plot = acq_list
-    match n_parm:
-        # case 1:
-        #     if disable == False:
-        #         values.parameter1.append(random.randint(parmMin, parmMax))
-        #         values.Acqy.append(random.randint(costMin, costMax))
-        #     layout = go.Layout(xaxis = dict(title='Parameter',range=[parmMin, parmMax]),
-        #         yaxis=dict(title='Cost',range=[min(values.Acqy), max(values.Acqy)]))  #,title='Gaussian Process')
-        #     data = go.Scatter(x=list(values.parameter1), y=list(values.Acqy), name='gp', mode="lines+markers")
-        #     return {'data':[data], 'layout':layout}
+            values.HistGP = [data, data_1]
+            return {'data':values.HistGP, 'layout':layout}
         
         case 2:
             if time_inlet is not None:
                 if data_plot[0]:
-                    data_plot_arr = np.array(data_plot).flatten()
-                    cost = data_plot_arr[:len(data_plot_arr)//3]
-                    parm1 = data_plot_arr[len(data_plot_arr)//3:2*len(data_plot_arr)//3]
-                    parm2 = data_plot_arr[2*len(data_plot_arr)//3:len(data_plot_arr)]
-                    values.parameter1 = list(parm1.flatten())
-                    values.parameter2 = list(parm2.flatten())
-
-            layout = go.Layout(scene = dict(xaxis_title='Parameter 1', yaxis_title='Parameter 2',
-                            zaxis_title='Cost'), width=600, margin=dict(r=10, b=10, l=10, t=10))
-            if values.Acq_data_plot is not None:
-                nx,ny = (20,20)
-                x = np.linspace(0,0.85, nx)
-                y= np.linspace(0,0.85, ny)
+                    values.parameter1.append(data_plot[1])
+                    values.parameter2.append(data_plot[2])
+                    values.GPy.append(data_plot[0])             
+            layout = go.Layout(scene = dict(xaxis_title='Parameter 1',yaxis_title='Parameter 2',
+                                zaxis_title='Cost'), width=600, margin=dict(r=10, b=10, l=10, t=10))
+            data=go.Scatter3d(x=list(values.parameter1), y=list(values.parameter2), z=list(values.GPy), 
+                              name='cost_samples', mode="markers", )
+            values.HistGP= [data]
+            if values.GP_data_plot is not None:
+                nx,ny = (30,30)
+                x = np.linspace(0,85, nx)
+                y= np.linspace(0,85, ny)
                 xv, yv = np.meshgrid(x,y)
-                data = go.Mesh3d(x=xv.flatten(), y=yv.flatten(), z=list(values.Acq_data_plot), name='Acq',opacity=0.50)
-                return {'data':[data], 'layout':layout} 
+                data_1 = go.Mesh3d(x=xv.flatten(), y=yv.flatten(), z=list(values.GP_data_plot), name='GP',opacity=0.50) 
+                values.HistGP= [data, data_1]
             
+            return {'data':values.HistGP, 'layout':layout} 
+        
 
+# @app.callback(Output(component_id="live_Acq", component_property="figure"), 
+#               Input('graph-update', 'n_intervals'))   
+# def update_graph(n):
+
+#     [parmMin, parmMax]= config['Optimization']['range'][0]
+#     [costMin, costMax]= config['Optimization']['range_cost']
+#     n_parm = config['Optimization']['n_parms']
+
+#     data_plot, time_inlet = inlet.pull_sample(timeout=0.2)
+#     data_acq, time_inlet_acq = inlet_acq.pull_chunk(timeout=0.2)
+#     if len(time_inlet_acq):
+#         acq_list = [i[0] for i in data_acq]
+#         values.Acq_data_plot = acq_list
+#     match n_parm:
+#         # case 1:
+#         #     if disable == False:
+#         #         values.parameter1.append(random.randint(parmMin, parmMax))
+#         #         values.Acqy.append(random.randint(costMin, costMax))
+#         #     layout = go.Layout(xaxis = dict(title='Parameter',range=[parmMin, parmMax]),
+#         #         yaxis=dict(title='Cost',range=[min(values.Acqy), max(values.Acqy)]))  #,title='Gaussian Process')
+#         #     data = go.Scatter(x=list(values.parameter1), y=list(values.Acqy), name='gp', mode="lines+markers")
+#         #     return {'data':[data], 'layout':layout}
+        
+#         case 2:
+#             if time_inlet is not None:
+#                 if data_plot[0]:
+#                     values.parameter1.append(data_plot[1])
+#                     values.parameter2.append(data_plot[2])
+
+#             layout = go.Layout(scene = dict(xaxis_title='Parameter 1', yaxis_title='Parameter 2',
+#                             zaxis_title='Cost'), width=600, margin=dict(r=10, b=10, l=10, t=10))
+#             if values.Acq_data_plot is not None:
+#                 nx,ny = (30,30)
+#                 x = np.linspace(0,85, nx)
+#                 y= np.linspace(0,85, ny)
+#                 xv, yv = np.meshgrid(x,y)
+#                 data = go.Mesh3d(x=xv.flatten(), y=yv.flatten(), z=list(values.Acq_data_plot), name='Acq',opacity=0.50)
+#                 return {'data':[data], 'layout':layout} 
+            
 
 @app.callback(Output(component_id="live_ECG", component_property="figure"), 
               Input('graph-update', 'n_intervals'))   
@@ -288,47 +276,35 @@ def update_graph(n):
               Input('graph-update', 'n_intervals'))   
 def update_graph(n):
     n_parm = config['Optimization']['n_parms']
-    data_plot, time_inlet = inlet.pull_chunk(timeout=0.2)
-    print(time_inlet)
-    print(len(time_inlet))
-    if n_parm == 2 and len(time_inlet) > 0:
-        data_plot_arr = np.array(data_plot).flatten()
-        cost = data_plot_arr[:len(data_plot_arr)//3]
-        parm1 = data_plot_arr[len(data_plot_arr)//3:2*len(data_plot_arr)//3]
-        parm2 = data_plot_arr[2*len(data_plot_arr)//3:len(data_plot_arr)]
-        values.parameter1 = list(parm1.flatten())
-        values.parameter1 = list(parm1.flatten())
+    data_plot, time_inlet = inlet.pull_sample(timeout=0.2)
     layout = go.Layout(xaxis = dict(title='Iterations'),yaxis=dict(title='Parameter value'),title='Parameters')
-    data1 = go.Scatter(y=values.parameter1, name='Parameter1', mode="lines")
-    data2 = go.Scatter(y=values.parameter2, name='Parameter2', mode="lines")  
-    return {'data': [data1,data2], 'layout': layout}
-    if True:
+    if time_inlet is not None:
         if n_parm>=1: 
+           values.parameter1.append(data_plot[1])
            data1 = go.Scatter(y=values.parameter1, name='Parameter1', mode="lines")
-           stamp= [data1]
+           values.HistParm = [data1]
            if n_parm>=2:
-
+                values.parameter2.append(data_plot[2])
                 data2 = go.Scatter(y=values.parameter2, name='Parameter2', mode="lines")  
-                stamp= [data1, data2]   
+                values.HistParm = [data1, data2]   
                 if n_parm>=3:
                     values.parameter3.append(data_plot[3])
                     data3 = go.Scatter(y=values.parameter3, name='Parameter3', mode="lines")  
-                    stamp= [data1, data2, data3] 
+                    values.HistParm = [data1, data2, data3] 
                     if n_parm>=4:
                         values.parameter4.append(data_plot[4])
                         data4 = go.Scatter(y=values.parameter4, name='Parameter4', mode="lines")  
-                        stamp= [data1, data2, data3, data4]
+                        values.HistParm = [data1, data2, data3, data4]
                         if n_parm>=5:
                             values.parameter5.append(data_plot[5])
                             data5 = go.Scatter(y=values.parameter5, name='Parameter5', mode="lines")  
-                            stamp= [data1, data2, data3, data4, data5]
+                            values.HistParm = [data1, data2, data3, data4, data5]
                             if n_parm>=6:
                                 values.parameter6.append(data_plot[6])
                                 data6 = go.Scatter(y=values.parameter6, name='Parameter6', mode="lines")  
-                                stamp= [data1, data2, data3, data4, data5, data6]
-
-        print(stamp)       
-    return {'data':stamp, 'layout':layout}
+                                values.HistParm = [data1, data2, data3, data4, data5, data6]
+       
+    return {'data':values.HistParm, 'layout':layout}
 
 
 # @app.callback(
@@ -376,4 +352,4 @@ def submit(n_submit, n_parm, GP, Acq, parmRanges, costMin, costMax):
         
 
 if __name__ == '__main__':
-    app.run_server(debug=False, port = 8080, host='0.0.0.0')
+    app.run_server(debug=True, port = 8080) # host='0.0.0.0')
